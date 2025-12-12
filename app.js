0a57068b29edb62f7f80edb87c875d79644d3746
Nik4oo1 <134060884+Nik4oo1@users.noreply.github.com>
2025-12-12 16:08:01 +0200

    Add Task2 component with children prop demo

---
+++ app.js
@@ -0,0 +1,15 @@
+return (
+  <div className="App">
+    <h1>Props Overview – `children` demo</h1>
+
+    <Task2 
+      firstName="Ivan" 
+      lastName="Danovski" 
+      title="Teacher" 
+      image={reactLogo}
+    >
+      This is the "children" prop
+    </Task2>
+
+  </div>
+);
